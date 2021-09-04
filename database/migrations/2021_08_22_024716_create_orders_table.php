10270e46fa0766297b3304913307921012473793
herry88 <herry081288@gmail.com>
2021-09-04 13:12:05 +0700

    cart

--- database/migrations/2021_08_22_024716_create_orders_table.php
+++ database/migrations/2021_08_22_024716_create_orders_table.php
@@ -15,6 +15,11 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('produk_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('jumlah');
+            $table->bigInteger('sub_total')->nullable();
+            $table->bigInteger('status')->default('0');
             $table->timestamps();
         });
     }
