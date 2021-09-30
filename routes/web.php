814a45fb0979056240568b15fc374a32fb10edba
herry88 <herry081288@gmail.com>
2021-09-30 12:47:32 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return redirect()->route('home');
 });
 
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 //rute baru
 Route::resource('category', App\Http\Controllers\CategoryController::class)->middleware('is_admin');
 
